f464ba84a1c4a6b8acb6e695fd29d57fe9f2d751
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-07-24 18:02:24 -0700

    solve ex03

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -9,6 +9,17 @@ Create a function named numberOfVowels that will receive a string and return the
 
 const numberOfVowels = function (data) {
   // Put your solution here
+  let counter = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (const char of data) {
+    for (const vowel of vowels) {
+      if (char === vowel) {
+        counter++;
+        break;
+      }
+    }
+  }
+  return counter;
 };
 
 console.log(numberOfVowels("orange")); // 3
